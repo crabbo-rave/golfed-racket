a2bdf68cc39ca97b1253075d2eac6f7a55244bd4
crab <92041779+crabbo-rave@users.noreply.github.com>
2022-04-11 14:06:49 -0400

    stuff

--- golfed-gfizz.rkt
+++ golfed-gfizz.rkt
@@ -1,6 +1,3 @@
 #lang racket
 
-(require qi)(define(g-f t m)(for([a(range 1(+ 1 m))])(~>(t)dict-keys sep(pass(~>>(gcd a)(dict-has-key? t)))(><(dict-ref t _))string-append(if non-empty-string? _(gen a))println)))
-
-(define tbl '((3 . "Fizz") (5 . "Buzz") (7 . "Bizz")))
-(g-f tbl 50)
+(require qi)(define t'((3 . "Fizz")(5 . "Buzz")(7 . "Bizz")))(for([a 101])(~>(t)dict-keys sep(pass(~>>(gcd a)(dict-has-key? t)))(><(dict-ref t _))string-append(if non-empty-string? _(gen a))println))
