201a31f9ddb396c79572898d5d1b020c1aa81572
crab <92041779+crabbo-rave@users.noreply.github.com>
2022-04-14 10:21:50 -0400

    Stugg

--- golfed-gfizz.rkt
+++ golfed-gfizz.rkt
@@ -1,3 +1,1 @@
-#lang racket
-
-(require qi)(define t'((3 . "Fizz")(5 . "Buzz")(7 . "Bizz")))(for([a 101])(~>(t)dict-keys sep(pass(~>>(gcd a)(dict-has-key? t)))(><(dict-ref t _))string-append(if non-empty-string? _(gen a))println))
+#!racket ((λ(t)(for([a 101])((λ(m)(displayln(if(equal?""m)(+ a 1)m)))(string-join(for/list([k(dict-keys t)])(dict-ref t(gcd(+ 1 a)k)""))""))))'((3 ."Fizz")(5 ."Buzz")(7 ."Bizz")))
